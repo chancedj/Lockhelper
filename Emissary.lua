bd1e8ac735b26c74c642170d40c262ff4eb0385f
Stinkfist <chancedj@yahoo.com>
2017-10-16 17:50:00 -0400

    create emissary table
    create hardcoded emissary table since bonus is not reliable for finding completed emissary quests.

--- Emissary.lua
+++ Emissary.lua
@@ -18,6 +18,7 @@ local GetQuestBountyInfoForMapID, GetQuestLogTitle, GetQuestLogIndexByID,
         GetQuestResetTime, GetServerTime                                 -- blizzard api
 
 local EMISSARY_MAP_ID = 1014;
+
       
 function addon:Lockedout_BuildEmissary( realmName, charNdx )
     local emissaries = {}; -- initialize world boss table;
@@ -25,9 +26,31 @@ function addon:Lockedout_BuildEmissary( realmName, charNdx )
     
     local bounties = GetQuestBountyInfoForMapID( EMISSARY_MAP_ID );
 
+    local test = { };
+    
+    table.insert( test, "48642" ); -- argussian reach
+    table.insert( test, "48641" ); -- armies of the legionfall
+    table.insert( test, "48639" ); -- armies of the light
+    table.insert( test, "42420" ); -- court of farondis
+    table.insert( test, "42233" ); -- highmountain tribes
+    table.insert( test, "42170" ); -- the dreamweavers
+    table.insert( test, "43179" ); -- kirin tor of dalaran
+    table.insert( test, "42421" ); -- the nightfallen
+    table.insert( test, "42234" ); -- the valajar   
+    table.insert( test, "42422" ); -- the wardens
+    ---[[
+    for ndx, questId in next, test do
+        local timeleft = C_TaskQuest.GetQuestTimeLeftMinutes( questId );
+        print( "[" .. ndx .. "] QuestId: " .. questId .. " timeleft: " .. SecondsToTime( timeleft * 60 ) );
+        local a = GetQuestLogIndexByID( questId );
+        
+        print( "|cffffff00|Hquest:" .. questId .. "|h[%%s]|h|r" );
+    end
+    
+    local b = L["booger"];
+    --]]
     for i = 1, #bounties do
         local questId = bounties[ i ].questID;
-        
         local _, _, finished, numFulfilled, numRequired = GetQuestObjectiveInfo( questId, 1, false )
         local title = GetQuestLogTitle( GetQuestLogIndexByID( questId ) );
         
